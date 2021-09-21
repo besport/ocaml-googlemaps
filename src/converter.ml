38f2166b08d89d7ee6ecd947bcaa21d692bd46f0
Thibaut <tgudin@besport.com>
2021-09-21 17:37:53 +0200

    convert ocaml-googlemaps into a dune project

--- src/converter.ml
+++ src/converter.ml
@@ -1,7 +1,6 @@
 open Js_of_ocaml
 
 module Element = struct
-
   type t = Dom_html.element Js.t
 
   let t_to_js : t -> Ojs.t = Obj.magic
@@ -9,11 +8,9 @@ module Element = struct
   let t_of_js : Ojs.t -> t = Obj.magic
 
   let t_of_dom : Dom_html.element Js.t -> t = fun x -> x
-
 end
 
 module Date = struct
-
   type t = Js.date Js.t
 
   let t_to_js : t -> Ojs.t = Obj.magic
@@ -21,5 +18,4 @@ module Date = struct
   let t_of_js : Ojs.t -> t = Obj.magic
 
   let t_of_date : Js.date Js.t -> t = fun x -> x
-
 end
